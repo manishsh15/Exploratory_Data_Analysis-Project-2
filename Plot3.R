25cd4401216dd5c31df825b8ee05ee7e44d475bf
manishsh15 <manishsh15@users.noreply.github.com>
2016-09-18 01:08:15 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,19 +1,24 @@
+Exploratory Data Anaysis-Project2- Q3
 # Load ggplot2 library
 require(ggplot2)
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+
+# Loading provided datasets 
+NEI <- readRDS("~/Exploratory_Data_Analysis/Project_2/summarySCC_PM25.rds")
+SCC <- readRDS("~/Exploratory_Data_Analysis/Project_2/Source_Classification_Code.rds")
+
 # Sampling
 NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
+
 # Baltimore City, Maryland == fips
 MD <- subset(NEI, fips == 24510)
 MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
+
 # Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
 # which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
 # Which have seen increases in emissions from 1999–2008? 
 # Use the ggplot2 plotting system to make a plot answer this question.
-# Generate the graph in the same directory as the source code
-png(filename='~/Exploratory_Data_Analysis/Assignment_2/plot3.png', width=800, height=500, units='px')
+# Generate the graph
+png(filename='~/Exploratory_Data_Analysis/Project_2/plot3.png', width=800, height=500, units='px')
 
 
 ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
