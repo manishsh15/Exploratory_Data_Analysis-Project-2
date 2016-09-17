09edcee7658141b77b4b545e62bef5667bf75fb6
manishsh15 <manishsh15@users.noreply.github.com>
2016-09-18 01:16:45 +0530

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,20 +1,23 @@
+Exploratory Data Analysis-Project2-Q5
+
 # Load ggplot2 library
 library(ggplot2)
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/summarySCC_PM25.rds")
-SCC <- readRDS("~/Exploratory_Data_Analysis/Assignment_2/Source_Classification_Code.rds")
+
+# Loading datasets 
+NEI <- readRDS("~/Exploratory_Data_Analysis/Project_2/summarySCC_PM25.rds")
+SCC <- readRDS("~/Exploratory_Data_Analysis/Project_2/Source_Classification_Code.rds")
 
 NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
 
 # Baltimore City, Maryland == fips
 MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+
 # Aggregate
 MD.df <- aggregate(MD.onroad[, 'Emissions'], by=list(MD.onroad$year), sum)
 colnames(MD.df) <- c('year', 'Emissions')
 # How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City? 
-
-# Generate the graph in the same directory as the source code
-png('~/Exploratory_Data_Analysis/Assignment_2/plot5.png')
+# Generate the graph 
+png('~/Exploratory_Data_Analysis/Project_2/plot5.png')
 
 ggplot(data=MD.df, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year)) + guides(fill=F) + 
     ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
