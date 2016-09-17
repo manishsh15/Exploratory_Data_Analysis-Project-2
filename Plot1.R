03b1534bec27c5549d50e3e0a2833f6286dfbd1a
manishsh15 <manishsh15@users.noreply.github.com>
2016-09-18 01:21:15 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,5 @@
 Exploratory Data Analysis Project 2- Q1
+
 # Loading datasets 
 NEI <- readRDS("~/Exploratory_Data_Analysis/Project_2/summarySCC_PM25.rds")
 SCC <- readRDS("~/Exploratory_Data_Analysis/Project_2/Source_Classification_Code.rds")
@@ -13,7 +14,6 @@ Emissions$PM <- round(Emissions[,2]/1000,2)
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999,2002,2005, 
 # and 2008.
-
 # Generate the graph 
 png(filename='~/Exploratory_Data_Analysis/Project_2/plot1.png')
 barplot(Emissions$PM, names.arg=Emissions$Group.1, 
